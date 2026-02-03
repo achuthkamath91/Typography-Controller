cf56b11ae7545bce015cead3863df25851c7a5dd
Achuth Kamath <achuth.138208@gmail.com>
2026-02-02 19:57:29 -0500

    Update: package.json

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,11 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   build: {
-    outDir: "dist/web",
     lib: {
       entry: "src/typography-controller.js",
       name: "TypographyController",
       fileName: "typography-controller"
-    }
+    },
+     outDir: "dist/web",
   }
 });
